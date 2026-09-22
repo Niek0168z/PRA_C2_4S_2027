223a269dbafee1122703b481a89f3019b30bbf80
Niek Neve <d317593@edu.curio.nl>
2026-09-22 14:24:20 +0200

    Ticket_01 finised
    Css en HTML aangepast

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -34,14 +34,11 @@
 
         </div>
 
-        <div class="row">
-            <x-footer/>
-        </div>
-
     </div>
 
-
 </div>
+
+<x-footer/>
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
